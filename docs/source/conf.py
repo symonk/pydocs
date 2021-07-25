4f68ad8a4b399ec56357c567ac8ba07937e7f82f
symonk <jackofspaces@gmail.com>
2021-07-25 13:41:43 +0100

    adding /source dir for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-master_doc = "index"
+master_doc = "source/index"
 
 html_theme = 'sphinx_rtd_theme'
 
