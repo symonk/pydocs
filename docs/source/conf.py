6b3ac5403e3ba225e1b06a68fc1587609b1ca5ec
symonk <jackofspaces@gmail.com>
2021-07-26 19:05:15 +0100

    update conf project

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'pydocs'
+project = 'Python Learning Materials'
 copyright = '2021, Simon Kerr'
 author = 'Simon Kerr'
 
